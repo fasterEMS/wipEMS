8affa601f8db37a4f180af6fc9237f61208e6239
Yifeng Li <tomli@tomli.me>
2024-12-05 16:54:16 +0000

    Engine_Ext_Dispersive: support Tiling engine.
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- FDTD/extensions/engine_ext_dispersive.cpp
+++ FDTD/extensions/engine_ext_dispersive.cpp
@@ -21,6 +21,8 @@
 
 Engine_Ext_Dispersive::Engine_Ext_Dispersive(Operator_Ext_Dispersive* op_ext_disp) : Engine_Extension(op_ext_disp)
 {
+	m_TilingSupported = true;
+
 	m_Op_Ext_Disp = op_ext_disp;
 	int order = m_Op_Ext_Disp->m_Order;
 	curr_ADE = new FDTD_FLOAT**[order];
@@ -71,10 +73,108 @@ Engine_Ext_Dispersive::~Engine_Ext_Dispersive()
 
 	delete[] volt_ADE;
 	volt_ADE=NULL;
+
+	m_cellmap.clear();
+}
+
+void Engine_Ext_Dispersive::InitializeTiling(const Tiling::Plan3D& plan)
+{
+	for (const Tiling::TileList3D& stage : plan)
+	{
+		for (const Tiling::Tile3D& tile : stage)
+		{
+			for (const Tiling::Subtile3D& subtile : tile)
+			{
+				for (const Tiling::Range3D<>& range : subtile)
+				{
+					InitializeTilingImpl(range);
+				}
+			}
+		}
+	}
+
+	if (g_settings.getOption("verbose").as<unsigned int>() > 0)
+	{
+		for (int o = 0; o < m_Op_Ext_Disp->m_Order; o++)
+		{
+			std::cout << "Engine_Ext_Dispersive::InitializeTiling: ";
+			std::cout << "order " << o << " has "
+			          << m_cellmap[o].size() << " cells\n";
+		}
+	}
+}
+
+// Before simulation starts, Engine_Tiling tells us how the space is
+// partitioned into ranges of tiles via a data structure called "Plan".
+// Here we check all ranges in the plan if this range overlaps with
+// the range controlled by us, we append the affected ranges into a
+// hash table at m_cellmap[o], otherwise the table is empty. It allows
+// us to immediately know where to update when Engine_Tiling passses
+// a tile to us.
+void Engine_Ext_Dispersive::InitializeTilingImpl(Tiling::Range3D<> range)
+{
+	// m_cellmap itself is a vector with a length determined by the
+	// material physical model's order. The hash tables are located
+	// at m_cellmap[o].
+	m_cellmap.resize(m_Order);
+
+	for (int o=0;o<m_Op_Ext_Disp->m_Order;++o)
+	{
+		if (!m_Op_Ext_Disp->m_volt_ADE_On[o] &&
+		    !m_Op_Ext_Disp->m_curr_ADE_On[o])
+		{
+			continue;
+		}
+
+		unsigned int **pos = m_Op_Ext_Disp->m_LM_pos[o];
+
+		for (unsigned int i=0; i<m_Op_Ext_Disp->m_LM_Count.at(o); ++i)
+		{
+			if (InsideTile(range, pos, i))
+				m_cellmap[o][range].push_back(
+					{i, pos[0][i], pos[1][i], pos[2][i]}
+				);
+		}
+	}
+}
+
+// Whether the target cell is inside the tile that is currently
+// being processed.
+bool Engine_Ext_Dispersive::InsideTile(
+	Tiling::Range3D<> range,
+	unsigned int** target,
+	unsigned int i
+)
+{
+	for (unsigned int n = 0; n < 3; n++)
+	{
+		if (target[n][i] < range.first[n] || target[n][i] > range.last[n])
+			return false;
+	}
+
+	return true;
 }
 
 template <typename EngineType>
-void Engine_Ext_Dispersive::Apply2VoltagesImpl(EngineType* eng)
+void Engine_Ext_Dispersive::Apply2VoltagesImpl(
+	EngineType* eng,
+	std::array<unsigned int, 3> pos,
+	std::array<float, 3> volt_ADE
+)
+{
+	eng->EngineType::SetVolt(0, pos[0], pos[1], pos[2],
+		eng->EngineType::GetVolt(0, pos[0], pos[1], pos[2]) - volt_ADE[0]
+	);
+	eng->EngineType::SetVolt(1, pos[0], pos[1], pos[2],
+		eng->EngineType::GetVolt(1, pos[0], pos[1], pos[2]) - volt_ADE[1]
+	);
+	eng->EngineType::SetVolt(2, pos[0], pos[1], pos[2],
+		eng->EngineType::GetVolt(2, pos[0], pos[1], pos[2]) - volt_ADE[2]
+	);
+}
+
+// Used for Basic, SSE, and SSE_Compressed engines.
+void Engine_Ext_Dispersive::Apply2Voltages()
 {
 	for (int o=0;o<m_Op_Ext_Disp->m_Order;++o)
 	{
@@ -84,26 +184,59 @@ void Engine_Ext_Dispersive::Apply2VoltagesImpl(EngineType* eng)
 
 		for (unsigned int i=0; i<m_Op_Ext_Disp->m_LM_Count.at(o); ++i)
 		{
-			eng->EngineType::SetVolt(0,pos[0][i],pos[1][i],pos[2][i],
-				eng->EngineType::GetVolt(0,pos[0][i],pos[1][i],pos[2][i]) - volt_ADE[o][0][i]
-			);
-			eng->EngineType::SetVolt(1,pos[0][i],pos[1][i],pos[2][i],
-				eng->EngineType::GetVolt(1,pos[0][i],pos[1][i],pos[2][i]) - volt_ADE[o][1][i]
-			);
-			eng->EngineType::SetVolt(2,pos[0][i],pos[1][i],pos[2][i],
-				eng->EngineType::GetVolt(2,pos[0][i],pos[1][i],pos[2][i]) - volt_ADE[o][2][i]
+			ENG_DISPATCH_ARGS(
+				Apply2VoltagesImpl,
+				{pos[0][i], pos[1][i], pos[2][i]},
+				{volt_ADE[o][0][i], volt_ADE[o][1][i], volt_ADE[o][2][i]}
 			);
 		}
 	}
 }
 
-void Engine_Ext_Dispersive::Apply2Voltages()
+// Only used by Tiling engine.
+void Engine_Ext_Dispersive::Apply2Voltages(int timestep, Tiling::Range3D<> range)
 {
-	ENG_DISPATCH(Apply2VoltagesImpl);
+	for (int o=0;o<m_Op_Ext_Disp->m_Order;++o)
+	{
+		if (m_Op_Ext_Disp->m_volt_ADE_On[o]==false)
+			continue;
+		if (m_cellmap[o].count(range) == 0)
+			continue;
+
+		for (const std::array<unsigned int, 4>& val : m_cellmap[o].at(range))
+		{
+			unsigned int idx = val[0];
+			std::array<unsigned int, 3> pos = {val[1], val[2], val[3]};
+
+			ENG_DISPATCH_ARGS(
+				Apply2VoltagesImpl,
+				pos,
+				{volt_ADE[o][0][idx], volt_ADE[o][1][idx], volt_ADE[o][2][idx]}
+			);
+		}
+	}
 }
 
 template <typename EngineType>
-void Engine_Ext_Dispersive::Apply2CurrentImpl(EngineType* eng)
+void Engine_Ext_Dispersive::Apply2CurrentImpl(
+	EngineType* eng,
+	std::array<unsigned int, 3> pos,
+	std::array<float, 3> curr_ADE
+)
+{
+	eng->EngineType::SetCurr(0, pos[0], pos[1], pos[2],
+		eng->EngineType::GetCurr(0, pos[0], pos[1], pos[2]) - curr_ADE[0]
+	);
+	eng->EngineType::SetCurr(1, pos[0], pos[1], pos[2],
+		eng->EngineType::GetCurr(1, pos[0], pos[1], pos[2]) - curr_ADE[1]
+	);
+	eng->EngineType::SetCurr(2, pos[0], pos[1], pos[2],
+		eng->EngineType::GetCurr(2, pos[0], pos[1], pos[2]) - curr_ADE[2]
+	);
+}
+
+// Used for Basic, SSE, and SSE_Compressed engines.
+void Engine_Ext_Dispersive::Apply2Current()
 {
 	for (int o=0;o<m_Op_Ext_Disp->m_Order;++o)
 	{
@@ -113,20 +246,35 @@ void Engine_Ext_Dispersive::Apply2CurrentImpl(EngineType* eng)
 
 		for (unsigned int i=0; i<m_Op_Ext_Disp->m_LM_Count.at(o); ++i)
 		{
-			eng->EngineType::SetCurr(0,pos[0][i],pos[1][i],pos[2][i],
-				eng->EngineType::GetCurr(0,pos[0][i],pos[1][i],pos[2][i]) - curr_ADE[o][0][i]
-			);
-			eng->EngineType::SetCurr(1,pos[0][i],pos[1][i],pos[2][i],
-				eng->EngineType::GetCurr(1,pos[0][i],pos[1][i],pos[2][i]) - curr_ADE[o][1][i]
-			);
-			eng->EngineType::SetCurr(2,pos[0][i],pos[1][i],pos[2][i],
-				eng->EngineType::GetCurr(2,pos[0][i],pos[1][i],pos[2][i]) - curr_ADE[o][2][i]
+			ENG_DISPATCH_ARGS(
+				Apply2CurrentImpl,
+				{pos[0][i], pos[1][i], pos[2][i]},
+				{curr_ADE[o][0][i], curr_ADE[o][1][i], curr_ADE[o][2][i]}
 			);
 		}
 	}
 }
 
-void Engine_Ext_Dispersive::Apply2Current()
+// Only used by Tiling engine.
+void Engine_Ext_Dispersive::Apply2Current(int timestep, Tiling::Range3D<> range)
 {
-	ENG_DISPATCH(Apply2CurrentImpl);
+	for (int o=0;o<m_Op_Ext_Disp->m_Order;++o)
+	{
+		if (m_Op_Ext_Disp->m_curr_ADE_On[o]==false)
+			continue;
+		if (m_cellmap[o].count(range) == 0)
+			continue;
+
+		for (const std::array<unsigned int, 4>& val : m_cellmap[o].at(range))
+		{
+			unsigned int idx = val[0];
+			std::array<unsigned int, 3> pos = {val[1], val[2], val[3]};
+
+			ENG_DISPATCH_ARGS(
+				Apply2CurrentImpl,
+				pos,
+				{curr_ADE[o][0][idx], curr_ADE[o][1][idx], curr_ADE[o][2][idx]}
+			);
+		}
+	}
 }
