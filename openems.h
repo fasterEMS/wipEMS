8843a78601e00006fba0965ad376baab3be802bd
Yifeng Li <tomli@tomli.me>
2024-12-05 16:52:48 +0000

    FDTD: Add Tiling engine type definitions
    This commit adds definitions and calling code related to TILING_V1
    and EngineType_Tiling_V1, so the main openEMS can find the new
    Tiling engine.
    
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- openems.h
+++ openems.h
@@ -157,7 +157,7 @@ protected:
 #ifdef MPI_SUPPORT
 	enum EngineType {EngineType_Basic, EngineType_SSE, EngineType_SSE_Compressed, EngineType_Multithreaded, EngineType_MPI};
 #else
-	enum EngineType {EngineType_Basic, EngineType_SSE, EngineType_SSE_Compressed, EngineType_Multithreaded};
+	enum EngineType {EngineType_Basic, EngineType_SSE, EngineType_SSE_Compressed, EngineType_Multithreaded, EngineType_Tiling_V1};
 #endif
 	EngineType m_engine;
 	unsigned int m_engine_numThreads;
